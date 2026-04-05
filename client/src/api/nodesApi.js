3fc9e4e1431330777ddfc60694e26e30172e4051
Aman Singh <amansinghvns29@gmail.com>
2026-04-05 12:11:28 +0530

    Remove the localhost

--- client/src/api/nodesApi.js
+++ client/src/api/nodesApi.js
@@ -1,16 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 const BASE =
   import.meta.env.VITE_MASTER_URL ||
-  'http://localhost:4000' ||
-  'https://service-monitor-mni6.onrender.com';
+  "https://service-monitor-mni6.onrender.com";
 
 const api = axios.create({
   baseURL: BASE,
 });
 
 export const fetchNodes = () =>
-  api.get('/api/nodes').then((r) => r.data.nodes);
+  api.get("/api/nodes").then((r) => r.data.nodes);
 
 export const fetchNode = (id) =>
   api.get(`/api/nodes/${id}`).then((r) => r.data.node);
